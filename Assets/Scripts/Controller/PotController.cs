611a5ca048f47a15c6446ab9a4cdcd3c79287560
bambong <alsrbs2gh@gmail.com>
2023-07-14 16:02:32 +0900

    도자기 컨트롤러 추가

---
+++ Assets/Scripts/Controller/PotController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public enum PotType 
+{
+    Type_1,
+    Type_2,
+    Type_3,
+    Type_4,
+    Type_5,
+    Type_6
+}
+
+
+public class PotController : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    public PotType myType;
+    [HideInInspector]
+    public RectTransform rect;
+    [HideInInspector]
+    public Vector3 startPos;
+    [HideInInspector]
+    public Transform prevParent;
+    private void Start()
+    {
+        rect = GetComponent<RectTransform>();
+        prevParent = transform.parent;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        StartPosUpdate();
+        prevParent = transform.parent;
+        transform.SetParent(GameManager.Instance.elementMoveParent);
+        transform.SetAsLastSibling();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rect.transform.position = eventData.position;
+    }
+
+    private void StartPosUpdate()
+    {
+        startPos = rect.transform.position;
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        var gr = GameManager.Instance.graphicRaycaster;
+        List<RaycastResult> raycastResults = new List<RaycastResult>();
+        gr.Raycast(eventData,raycastResults);
+        foreach(var item in raycastResults)
+        {
+            //if(item.gameObject.CompareTag("Frame"))
+            //{
+            //    if(curFrame != null)
+            //    {
+            //        curFrame.curElement = null;
+            //        curFrame = null;
+            //    }
+            //    item.gameObject.GetComponent<FrameController>().OnDrop(this);
+            //    PrefabsManager.Instance.elementSpawner[(int)myType].Generate();
+            //    return;
+            //}
+        }
+        rect.transform.position = startPos;
+        rect.transform.SetParent(prevParent);
+    }
+
+}
